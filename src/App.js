d3a246f15c4dd8d50a4864c8e833ba44cacf3983
ecetokos <60781728+ecetokos@users.noreply.github.com>
2022-08-13 22:17:09 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from './components/Left/Navbar';
+//import Middle from './components/Middle/Middle';
+//import Right from './components/Right/Right';
+
+function App() {
+  return (
+   <div >
+    <Navbar/>
+   </div>
+  );
+}
+
+export default App;
---
+++ src/components/Left/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+//import { Button, Stack } from 'react-bootstrap';
+
+
+
+
+const Navbar = () => {
+  return (
+   
+   <div>
+    
+   </div>
+  )
+}
+
+export default Navbar
